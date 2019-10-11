6f223767c084d9039df7879ba415b3ab3d0ae199
Brian Hafner <bhafner@Mac-f40f2434ab72.local>
2019-10-11 06:39:13 -0700

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react';
+import SearchBar from './SearchBar'
+
+const App = () => {
+   return <div className='ui container' style={{'marginTop': '10px'}}><SearchBar/></div>
+}
+
+export default App;
